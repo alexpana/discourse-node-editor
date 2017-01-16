fdcb3c32b7d31c187508c4b2e0525455cfdf7e91
Alexandru Pana <alexandru.pana@manabreakstdios.com>
2017-01-17 01:12:00 +0200

    Add fonts and initial node

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -5,35 +5,50 @@ import ui.editor.NodeUI;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Launcher {
 
-	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, IOException, FontFormatException {
+        initLookAndFeel();
 
-		DarculaLaf.initInputMapDefaults(UIManager.getDefaults());
-		UIManager.setLookAndFeel(new DarculaLaf());
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		frame.setSize(800, 600);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-		frame.setTitle("Discourse 0.1");
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setTitle("Discourse 0.1");
 
-		frame.getContentPane().setLayout(new BorderLayout());
-		JButton close = new JButton("Close");
-		close.setAction(new AbstractAction() {
-			{
-				putValue(Action.NAME, "Close");
-			}
+        frame.getContentPane().setLayout(new BorderLayout());
+        JButton close = new JButton("Close");
+        close.setAction(new AbstractAction() {
+            {
+                putValue(Action.NAME, "Close");
+            }
 
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Close clicked");
-			}
-		});
-		frame.getContentPane().add(close, BorderLayout.SOUTH);
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Close clicked");
+            }
+        });
+        frame.getContentPane().add(close, BorderLayout.SOUTH);
 
-		NodeEditor nodeEditor = new NodeEditor();
-		nodeEditor.addNode(new NodeUI());
-		frame.getContentPane().add(nodeEditor);
-	}
+        NodeEditor nodeEditor = new NodeEditor();
+        nodeEditor.addNode(new NodeUI());
+        frame.getContentPane().add(nodeEditor);
+    }
+
+    private static void initLookAndFeel() throws UnsupportedLookAndFeelException, FontFormatException, IOException {
+        DarculaLaf.initInputMapDefaults(UIManager.getDefaults());
+        UIManager.setLookAndFeel(new DarculaLaf());
+        loadFont("Font.OpenSans-ExtraBold", "fonts/OpenSans-ExtraBold.ttf");
+        loadFont("Font.OpenSans-Regular", "fonts/OpenSans-Regular.ttf");
+        loadFont("Font.FontAwesome", "fonts/FontAwesome.otf");
+    }
+
+    private static void loadFont(String resourceName, String filePath) throws FontFormatException, IOException {
+        InputStream inputStream = Launcher.class.getResourceAsStream(filePath);
+        Font font = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+        UIManager.getDefaults().put(resourceName, font);
+    }
 }
--- src/main/java/ui/editor/NodeUI.java
+++ src/main/java/ui/editor/NodeUI.java
@@ -2,8 +2,6 @@ package ui.editor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.AWTEventListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import static java.awt.AWTEvent.MOUSE_EVENT_MASK;
@@ -14,68 +12,107 @@ import static java.awt.AWTEvent.MOUSE_MOTION_EVENT_MASK;
  */
 public class NodeUI extends JPanel {
 
-	public NodeUI() {
-		setBorder(BorderFactory.createLineBorder(UIManager.getColor("Label.foreground")));
-		setLayout(new BorderLayout());
-		add(new Header(), BorderLayout.NORTH);
-	}
+    public static final Color BORDER_COLOR = new Color(0x333537);
 
-	public class Header extends JPanel {
+    public static final Color SPECULAR_COLOR = new Color(0x4C4F52);
 
-		private Point lastMousePosition;
+    public static final Color BACKGROUND_COLOR = new Color(0x424548);
 
-		private boolean isDragging = false;
+    public static final Color SLOT_BORDER = new Color(0x333637);
 
-		public Header() {
-			Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
-				MouseEvent mouseEvent = (MouseEvent) event;
-				Point mousePoint = mouseEvent.getLocationOnScreen();
-				if (mouseEvent.getID() == Event.MOUSE_DOWN) {
-					Point localPoint = new Point(mousePoint);
-					SwingUtilities.convertPointFromScreen(localPoint, this);
-					if (this.contains(localPoint)) {
-						beginDrag(mousePoint);
-					}
-				}
-				if (mouseEvent.getID() == Event.MOUSE_UP) {
-					endDrag();
-				}
-				if (event.getID() == Event.MOUSE_DRAG) {
-					updateDrag(mousePoint);
-				}
-			}, MOUSE_MOTION_EVENT_MASK | MOUSE_EVENT_MASK);
+    public static final Color SLOT_COLOR = new Color(0x5E6266);
 
-			setMinimumSize(new Dimension(100, 30));
-			setPreferredSize(new Dimension(100, 30));
-			setMaximumSize(new Dimension(2000, 30));
+    public static final int SLOT_SIZE = 13;
 
-			Label title = new Label("Title");
-			title.setBackground(Color.red);
-			title.setAlignment(SwingConstants.CENTER);
-			add(title);
-		}
+    public static final int SLOT_BORDER_WIDTH = 3;
 
-		private void beginDrag(Point mousePosition) {
-			lastMousePosition = mousePosition;
-			isDragging = true;
-		}
+    public NodeUI() {
+//        setBorder(BorderFactory.createLineBorder(UIManager.getColor("Label.foreground")));
+//        setLayout(new BorderLayout());
+        add(new Header(), BorderLayout.NORTH);
+    }
 
-		private void endDrag() {
-			isDragging = false;
-		}
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		private void updateDrag(Point newMousePosition) {
-			System.out.println(isDragging);
-			if (!isDragging) {
-				return;
-			}
-			int deltaX = newMousePosition.x - lastMousePosition.x;
-			int deltaY = newMousePosition.y - lastMousePosition.y;
+        g2d.setColor(BORDER_COLOR);
+        g2d.fillRoundRect(0, 0, getWidth() - SLOT_SIZE / 2, getHeight(), 4, 4);
 
-			NodeUI node = NodeUI.this;
-			System.out.println(deltaX + ", " + deltaY);
-			node.setLocation(node.getLocation().x + deltaX, node.getLocation().y + deltaY);
-			lastMousePosition = newMousePosition;
-		}
-	}
+        g2d.setColor(SPECULAR_COLOR);
+        g2d.fillRoundRect(2, 2, getWidth() - SLOT_SIZE / 2 - 4, getHeight() - 4, 4, 4);
+
+        g2d.setColor(BACKGROUND_COLOR);
+        g2d.fillRoundRect(2, 3, getWidth() - SLOT_SIZE / 2 - 4, getHeight() - 5, 4, 4);
+
+        Font font = (Font) UIManager.getDefaults().get("Font.OpenSans-ExtraBold");
+
+        g2d.setColor(new Color(0x738495));
+        g2d.setFont(font.deriveFont(13.0f));
+        g2d.drawString("Reply Choice", 10, 18);
+
+        g2d.setColor(SLOT_BORDER);
+        g2d.fillOval(getWidth() - SLOT_SIZE, 20, SLOT_SIZE, SLOT_SIZE);
+
+        g2d.setColor(SLOT_COLOR);
+        g2d.fillOval(getWidth() - SLOT_SIZE + SLOT_BORDER_WIDTH, 20 + SLOT_BORDER_WIDTH, SLOT_SIZE - 2 * SLOT_BORDER_WIDTH, SLOT_SIZE - 2 * SLOT_BORDER_WIDTH);
+
+    }
+
+    public class Header extends JPanel {
+
+        private Point lastMousePosition;
+
+        private boolean isDragging = false;
+
+        public Header() {
+            Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
+                MouseEvent mouseEvent = (MouseEvent) event;
+                Point mousePoint = mouseEvent.getLocationOnScreen();
+                if (mouseEvent.getID() == Event.MOUSE_DOWN) {
+                    Point localPoint = new Point(mousePoint);
+                    SwingUtilities.convertPointFromScreen(localPoint, this);
+                    if (this.contains(localPoint)) {
+                        beginDrag(mousePoint);
+                    }
+                }
+                if (mouseEvent.getID() == Event.MOUSE_UP) {
+                    endDrag();
+                }
+                if (event.getID() == Event.MOUSE_DRAG) {
+                    updateDrag(mousePoint);
+                }
+            }, MOUSE_MOTION_EVENT_MASK | MOUSE_EVENT_MASK);
+
+            setLayout(new BorderLayout());
+            setOpaque(false);
+            setMinimumSize(new Dimension(100, 20));
+            setPreferredSize(new Dimension(1000, 20));
+            setMaximumSize(new Dimension(2000, 20));
+        }
+
+        private void beginDrag(Point mousePosition) {
+            lastMousePosition = mousePosition;
+            isDragging = true;
+        }
+
+        private void endDrag() {
+            isDragging = false;
+        }
+
+        private void updateDrag(Point newMousePosition) {
+            System.out.println(isDragging);
+            if (!isDragging) {
+                return;
+            }
+            int deltaX = newMousePosition.x - lastMousePosition.x;
+            int deltaY = newMousePosition.y - lastMousePosition.y;
+
+            NodeUI node = NodeUI.this;
+            System.out.println(deltaX + ", " + deltaY);
+            node.setLocation(node.getLocation().x + deltaX, node.getLocation().y + deltaY);
+            lastMousePosition = newMousePosition;
+        }
+    }
 }
