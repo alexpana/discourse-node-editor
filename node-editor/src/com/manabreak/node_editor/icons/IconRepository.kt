6d9b1ef466ce0ad6ce2e3af407cb0f0086282b0e
Alexandru Pana <alexandru.pana@manabreakstdios.com>
2017-01-21 15:24:09 +0200

    Readd icon repository

---
+++ node-editor/src/com/manabreak/node_editor/icons/IconRepository.kt
@@ -0,0 +1,65 @@
+package com.manabreak.node_editor.icons
+
+import com.manabreak.node_editor.Theme.Companion.theme
+import com.manabreak.node_editor.icons.FontAwesomeGlyph.LIST
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.RenderingHints
+import java.awt.image.BufferedImage
+import java.awt.image.BufferedImage.TYPE_INT_ARGB
+import java.util.*
+import javax.swing.Icon
+import javax.swing.ImageIcon
+
+object IconRepository {
+
+    private val NODE_ICON_FONT_SIZE = 28.0f
+
+    private val NODE_ICON_SIZE = 40
+
+    private val cache = HashMap<String, BufferedImage>()
+
+    private var replyChoiceNodeIcon: Icon? = null
+
+    fun getReplyChoiceNodeIcon(): Icon? {
+        if (replyChoiceNodeIcon == null) {
+            replyChoiceNodeIcon = createReplyChoiceNodeIcon()
+        }
+        return replyChoiceNodeIcon
+    }
+
+    fun cacheIcon(name: String, icon: BufferedImage) {
+        cache.put(name, icon)
+    }
+
+    fun isCached(name: String): Boolean {
+        return cache.containsKey(name)
+    }
+
+    fun getIcon(name: String): BufferedImage {
+        return cache[name]!!
+    }
+
+    fun newFontAwesomeIcon(glyph: FontAwesomeGlyph, iconSize: Int, glyphSize: Int, color: Color, offsetX: Int, offsetY: Int): BufferedImage {
+        val image = BufferedImage(NODE_ICON_SIZE, NODE_ICON_SIZE, TYPE_INT_ARGB)
+        val g2d = image.graphics as Graphics2D
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        val font = theme.fontAwesome.deriveFont(NODE_ICON_FONT_SIZE)
+        g2d.font = font
+        g2d.color = theme.choiceNodeColor
+        g2d.drawString(LIST.representation, 6f, NODE_ICON_SIZE - 9f)
+        return image
+    }
+
+    private fun createReplyChoiceNodeIcon(): Icon {
+        val image = BufferedImage(NODE_ICON_SIZE, NODE_ICON_SIZE, TYPE_INT_ARGB)
+        val g2d = image.graphics as Graphics2D
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        val font = theme.fontAwesome.deriveFont(NODE_ICON_FONT_SIZE)
+        g2d.font = font
+        g2d.color = theme.choiceNodeColor
+        g2d.drawString(LIST.representation, 6f, NODE_ICON_SIZE - 9f)
+        g2d.dispose()
+        return ImageIcon(image)
+    }
+}
